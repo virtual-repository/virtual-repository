e2232a0a1f388be6bc7e116c0e5f2f6d19eedd3d
fabiosimeoni <fabio.simeoni@me.com>
2013-08-16 14:39:39 +0200

    test cleanup

--- virtual-repository/src/test/java/org/acme/IoTest.java
+++ virtual-repository/src/test/java/org/acme/IoTest.java
@@ -8,6 +8,8 @@ import java.io.StringWriter;
 import javax.xml.namespace.QName;
 
 import org.junit.Test;
+import org.virtualrepository.Property;
+import org.virtualrepository.RepositoryService;
 import org.virtualrepository.csv.CsvAsset;
 import org.virtualrepository.csv.CsvCodelist;
 import org.virtualrepository.sdmx.SdmxCodelist;
@@ -20,21 +22,10 @@ import com.thoughtworks.xstream.io.xml.StaxDriver;
 public class IoTest {
 
 	@Test
-	public void discoveredCsvCanTravelTheWire() throws Exception {
-	
-		CsvAsset csv = new CsvAsset("testid","testname");
-		CsvAsset parsed = xmlRoundTripOf(csv);
-		
-		System.out.println(csv);	
-		System.out.println(parsed);	
-		
-		assertEquals(csv,parsed);
-	}
-	
-	@Test
-	public void publishedCsvCanTravelTheWire() throws Exception {
+	public void csvCanTravelTheWire() throws Exception {
 	
 		CsvAsset csv = new CsvAsset("testid","testname",aService().name("repotest").get());
+		csv.properties().add(new Property("name","value"));
 		CsvAsset parsed = xmlRoundTripOf(csv);
 		
 		System.out.println(csv);	
@@ -44,23 +35,7 @@ public class IoTest {
 	}
 	
 	@Test
-	public void discoveredCsvCodelistsCanTravelTheWire() throws Exception {
-	
-		CsvCodelist codelist = new CsvCodelist("testid","testname",0);
-		
-		Column c = new Column(new QName("code"), new QName("code"), String.class);
-		codelist.setColumns(c);
-		
-		CsvCodelist parsed = xmlRoundTripOf(codelist);
-		
-		System.out.println(codelist);	
-		System.out.println(parsed);	
-		
-		assertEquals(codelist,parsed);
-	}
-	
-	@Test
-	public void publishedCsvCodelistsCanTravelTheWire() throws Exception {
+	public void csvCodelistsCanTravelTheWire() throws Exception {
 	
 		CsvCodelist codelist = new CsvCodelist("testid","testname",0,aService().name("repotest").get());
 		
@@ -78,19 +53,7 @@ public class IoTest {
 	
 	
 	@Test
-	public void discoveredSdmxCodelistCanTravelTheWire() throws Exception {
-	
-		SdmxCodelist codelist = new SdmxCodelist("urn","id", "1.0", "name");
-		SdmxCodelist parsed = xmlRoundTripOf(codelist);
-		
-		System.out.println(codelist);	
-		System.out.println(parsed);
-		
-		assertEquals(codelist,parsed);
-	}
-	
-	@Test
-	public void publishedSdmxCodelistCanTravelTheWire() throws Exception {
+	public void sdmxCodelistCanTravelTheWire() throws Exception {
 	
 		SdmxCodelist codelist = new SdmxCodelist("name",aService().name("repotest").get());
 		SdmxCodelist parsed = xmlRoundTripOf(codelist);
@@ -105,6 +68,7 @@ public class IoTest {
 	static <T> T xmlRoundTripOf(T o) throws Exception {
 
 		XStream x = new XStream(new StaxDriver());
+		x.omitField(RepositoryService.class, "proxy");
 		
 		StringWriter w = new StringWriter();
 		x.marshal(o, new PrettyPrintWriter(w));
